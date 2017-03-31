14b34eed4a14baf89327fd216886c3bffa6f54e6
Alex Mayfield <alexmax2742@gmail.com>
2017-03-30 23:31:45 -0400

    Render the console with DrawLists
    In order to figure out how many lines a console line takes up, you
    essentially follow the same steps as if you were actually rendering
    the line without actually rendering it.  This isn't necessarily taxing
    on the CPU, but requires that you either duplicate code, or have a weird
    function that renders or returns the height.
    
    I chose instead to implement a DrawList.  A DrawList is a vector of
    functions and their essential parameters (called edicts) that are meant
    to be iterated through and rendered all in one batch.  Creating the
    DrawList is much like rendering a bunch of related items in sequence,
    except you add edicts to the list instead of actually calling the real
    renderer functions.  At the time the DrawList is complete, you should
    also be aware of the true combined width and height of the entire batch
    and can thereafter set the width and height of the DrawList itself.
    
    Rendering of the console is simple at this point.  Iterate backwards
    through the console buffer and if a line doesn't have a DrawList,
    generate one.  Then, check the height of the DrawList and if you can
    fit it, render it.  Repeat until you hit the top of the screen.

--- src/c_console.cpp
+++ src/c_console.cpp
@@ -15,6 +15,7 @@
 //     Console.
 //
 
+#include <memory>
 #include <string>
 #include <array>
 
@@ -25,47 +26,56 @@
 #include "w_wad.h"
 #include "i_video.h"
 #include "v_video.h"
+#include "draw_list.h"
 
 namespace console
 {
+
+// A single line of the console, plus its associated DrawList.
+//
+// FIXME: DrawLists should be invalidated on resolution switch.
+struct BufferLine
+{
+    std::string line;
+    std::unique_ptr<video::DrawList> drawer;
+    BufferLine(): line(""), drawer(nullptr) { }
+};
 
 // Buffer to store past console lines.
 class Buffer
 {
 private:
-    std::array<std::string, 2048> line_buffer;
+    std::array<BufferLine, 2048> line_buffer;
     size_t head;
     size_t tail;
 public:
-    class const_iterator;
+    class iterator;
     Buffer() : head(0), tail(0) { };
     static Buffer& Instance();
     void AppendLines(const char* str);
     void Clear();
-    const std::string& GetLast() {
-        return this->line_buffer.at(this->tail);
-    }
-    const_iterator begin() const;
-    const_iterator end() const;
+    iterator begin();
+    iterator end();
 };
 
-class Buffer::const_iterator
+class Buffer::iterator
 {
 private:
-    const Buffer* buffer;
+    Buffer* buffer;
     size_t index;
 public:
-    bool operator!=(const const_iterator &b) const;
-    const_iterator& operator++();
-    const std::string& operator*() const;
-    const_iterator(const Buffer* buffer, size_t index) : buffer(buffer), index(index) { }
+    bool operator!=(iterator &b);
+    iterator& operator++();
+    iterator& operator--();
+    BufferLine& operator*();
+    iterator(Buffer* buffer, size_t index) : buffer(buffer), index(index) { }
 };
 
 // Get the singleton instance of the class
 Buffer& Buffer::Instance()
 {
-    static Buffer singleoton;
-    return singleoton;
+    static Buffer singleton;
+    return singleton;
 }
 
 // Append a string to the console buffer, splitting on newlines.
@@ -74,13 +84,14 @@ void Buffer::AppendLines(const char* str)
     const char* sb = str;
     const char* se = str;
 
-    std::string dest = std::move(this->line_buffer.at(this->head));
+    auto dest = std::move(this->line_buffer.at(this->head));
+    dest.drawer = nullptr;
 
     for(;;)
     {
         if (*se == '\n')
         {
-            dest.append(sb, se - sb);
+            dest.line.append(sb, se - sb);
             this->line_buffer.at(this->head) = std::move(dest);
             this->head += 1 % this->line_buffer.size();
             if (this->head == this->tail)
@@ -88,14 +99,14 @@ void Buffer::AppendLines(const char* str)
                 this->tail += 1 % this->line_buffer.size();
             }
 
-            dest = std::string();
+            dest = BufferLine();
 
             se += 1;
             sb = se;
         }
         else if (*se == '\0')
         {
-            dest.append(sb, se - sb);
+            dest.line.append(sb, se - sb);
             this->line_buffer.at(this->head) = std::move(dest);
             break;
         }
@@ -111,43 +122,50 @@ void Buffer::Clear()
 {
     this->head = 0;
     this->tail = 0;
-    this->line_buffer.at(0) = "";
+    this->line_buffer.at(0).line = "";
+    this->line_buffer.at(0).drawer = nullptr;
 }
 
 // Begin iterator
-Buffer::const_iterator Buffer::begin() const
+Buffer::iterator Buffer::begin()
 {
-    return const_iterator(this, 0);
+    return iterator(this, 0);
 }
 
 // End iterator
-Buffer::const_iterator Buffer::end() const
+Buffer::iterator Buffer::end()
 {
     // Our buffer always grows and never shrinks, so the only time
     // we will ever have a partway-filled buffer is if the tail
     // hasn't moved yet.
     if (tail == 0)
     {
-        return const_iterator(this, this->head);
+        return iterator(this, this->head);
     }
     else
     {
-        return const_iterator(this, this->line_buffer.size());
+        return iterator(this, this->line_buffer.size());
     }
 }
 
-bool Buffer::const_iterator::operator!=(const const_iterator &b) const
+bool Buffer::iterator::operator!=(iterator &b)
 {
     return this->index != b.index;
 }
 
-Buffer::const_iterator& Buffer::const_iterator::operator++()
+Buffer::iterator& Buffer::iterator::operator++()
 {
     this->index++;
     return *this;
 }
 
-const std::string& Buffer::const_iterator::operator*() const
+Buffer::iterator& Buffer::iterator::operator--()
+{
+    this->index--;
+    return *this;
+}
+
+BufferLine& Buffer::iterator::operator*()
 {
     size_t index = this->buffer->tail + this->index % this->buffer->line_buffer.size();
     return this->buffer->line_buffer[index];
@@ -185,44 +203,73 @@ void Draw()
 {
     V_DrawFilledBox(0, 0, SCREENWIDTH, SCREENHEIGHT / 2, 0);
 
-    auto buffer = Buffer::Instance();
+    auto& buffer = Buffer::Instance();
+    int cony = (SCREENHEIGHT / 2) - 8;
 
-    int x = 0, y = 0;
-    for (const std::string& line : buffer)
+    // Start at the end, and draw backwards.
+    for (auto it = buffer.end();it != buffer.begin();)
     {
-        if (line.empty())
+        --it;
+        auto& line = *it;
+
+        if (line.drawer == nullptr)
         {
-            continue;
+            // We don't have a drawlist, create one.
+            auto drawer = video::DrawList();
+            int x = 0, y = 0, max_width = 0, max_line_height = 0;
+
+            for (const char& c : line.line)
+            {
+                patch_t* patch = nullptr;
+                Font::const_iterator fit = ConsoleFont.find(c);
+                if (fit != ConsoleFont.end())
+                {
+                    auto letter = fit->second;
+                    patch = const_cast<patch_t*>(reinterpret_cast<const patch_t*>(letter.GetData()));
+                }
+                else
+                {
+                    // Use space as fallback.  If that doesn't work, crash.
+                    auto letter = ConsoleFont.at(32);
+                    patch = const_cast<patch_t*>(reinterpret_cast<const patch_t*>(letter.GetData()));
+                }
+
+                if (patch->width + x > SCREENWIDTH)
+                {
+                    // Force newline.
+                    x = 0;
+                    y += max_line_height;
+                    max_line_height = 0;
+                }
+
+                drawer.Add(V_DrawPatchDirect, patch, x, y);
+                x += patch->width;
+
+                if (x > max_width)
+                {
+                    max_width = x;
+                }
+                if (patch->height >= max_line_height)
+                {
+                    max_line_height = patch->height;
+                }
+            }
+
+            drawer.SetWidth(max_width);
+            drawer.SetHeight(y + max_line_height);
+
+            // DrawList constructed, assign it.
+            line.drawer = std::make_unique<video::DrawList>(drawer);
         }
 
-        for (const char& c : line)
+        if (cony - line.drawer->GetHeight() < 0)
         {
-            Font::const_iterator it = ConsoleFont.find(c);
-            if (it == ConsoleFont.end())
-            {
-                continue;
-            }
-
-            auto letter = it->second;
-            patch_t* patch = const_cast<patch_t*>(reinterpret_cast<const patch_t*>(letter.GetData()));
-
-            if (SCREENWIDTH - patch->width < x)
-            {
-                x = 0;
-                y += patch->height;
-            }
-
-            if (SCREENHEIGHT / 2 - patch->height < y)
-            {
-                return;
-            }
-
-            V_DrawPatchDirect(x, y, patch);
-            x += patch->width;
+            // Don't draw off the top of the screen.
+            break;
         }
 
-        y += 8;
-        x = 0;
+        line.drawer->Draw(0, cony - line.drawer->GetHeight());
+        cony -= line.drawer->GetHeight();
     }
 }
 
